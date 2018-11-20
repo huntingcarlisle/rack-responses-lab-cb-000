17d614e14555bcab40d610deaaef67b24e5c0311
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 03:33:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    resp.write Time.now
+    resp.write Time.now.hour
  
     # num_1 = Kernel.rand(1..20)
     # num_2 = Kernel.rand(1..20)
